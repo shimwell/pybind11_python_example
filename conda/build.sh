685463f81a0610dec6bf01621438230ac80794ad
shimwell <mail@jshimwell.com>
2022-05-17 23:20:49 +0100

    added compile for moab and dagmc

---
+++ conda/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+mkdir MOAB
+cd MOAB
+git clone  --single-branch --branch 5.3.1 --depth 1 https://bitbucket.org/fathomteam/moab.git
+mkdir build
+cd build
+cmake ../moab -DENABLE_HDF5=ON \
+                -DENABLE_NETCDF=ON \
+                -DENABLE_FORTRAN=OFF \
+                -DENABLE_BLASLAPACK=OFF \
+                -DBUILD_SHARED_LIBS=OFF \
+                -DCMAKE_INSTALL_PREFIX=/MOAB
+make -j
+make -j install
+cmake ../moab -DENABLE_HDF5=ON \
+                -DENABLE_PYMOAB=ON \
+                -DENABLE_FORTRAN=OFF \
+                -DBUILD_SHARED_LIBS=ON \
+                -DENABLE_BLASLAPACK=OFF \
+                -DCMAKE_INSTALL_PREFIX=/MOAB
+make -j install
+cd pymoab
+bash install.sh
+python setup.py install
+# the following rm command appears to remove libraries that are need to use
+# pymoab so this has been commented out for now
+# rm -rf /MOAB/moab /MOAB/build
+
+
+mkdir DAGMC
+cd DAGMC
+git clone --single-branch --branch v3.2.1 --depth 1 https://github.com/svalinn/DAGMC.git
+mkdir build
+cd build
+cmake ../DAGMC -DBUILD_TALLY=ON \
+                -DMOAB_DIR=/MOAB \
+                -DDOUBLE_DOWN=${build_double_down} \
+                -DBUILD_STATIC_EXE=OFF \
+                -DBUILD_STATIC_LIBS=OFF \
+                -DCMAKE_INSTALL_PREFIX=/DAGMC/ \
+                -DDOUBLE_DOWN_DIR=/double-down
+make -j install
+
+cd ..
+cd ..
+
+
+cd ..
+cd ..
+cd ..
+
+python -m pip install . -vvv
